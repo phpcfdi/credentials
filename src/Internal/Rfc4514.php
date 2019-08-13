953f43d108afefbde23236d2f40d7f9259107b85
Carlos C Soto <eclipxe13@gmail.com>
2019-08-13 16:41:55 -0500

    Create internal utility to escape according to RFC 4514

---
+++ src/Internal/Rfc4514.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpCfdi\Credentials\Internal;
+
+/**
+ * This class is created to replace escape strings and arrays according to RFC 4514
+ * @see https://www.ietf.org/rfc/rfc4514.txt
+ * @internal
+ */
+class Rfc4514
+{
+    public const LEAD_CHARS = [' ', '#'];
+
+    public const LEAD_REPLACEMENTS = ['\20', '\22'];
+
+    public const TRAIL_CHARS = [' '];
+
+    public const TRAIL_REPLACEMENTS = ['\20'];
+
+    public const INNER_CHARS = ['\\', '"', '+', ',', ';', '<', '=', '>'];
+
+    public const INNER_REPLACEMENTS = ['\5C', '\22', '\2b', '\2c', '\3b', '\3c', '\3d', '\3e'];
+
+    public function escape(string $subject): string
+    {
+        $prefix = '';
+        $sufix = '';
+        $firstChar = substr($subject, 0, 1);
+        if (in_array($firstChar, self::LEAD_CHARS, true)) {
+            $prefix = str_replace(self::LEAD_CHARS, self::LEAD_REPLACEMENTS, $firstChar);
+            $subject = substr($subject, 1);
+        }
+
+        $lastChar = substr($subject, -1);
+        if (in_array($lastChar, self::TRAIL_CHARS, true)) {
+            $sufix = str_replace(self::TRAIL_CHARS, self::TRAIL_REPLACEMENTS, $lastChar);
+            $subject = substr($subject, 0, -1);
+        }
+
+        return $prefix . str_replace(self::INNER_CHARS, self::INNER_REPLACEMENTS, $subject) . $sufix;
+    }
+
+    public function escapeArray(array $values): string
+    {
+        return implode(',', array_map(
+            function (string $name, string $value): string {
+                return $this->escape($name) . '=' . $this->escape($value);
+            },
+            array_keys($values),
+            $values
+        ));
+    }
+}
---
+++ tests/Unit/Internal/Rfc4514Test.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpCfdi\Credentials\Tests\Unit\Internal;
+
+use PhpCfdi\Credentials\Internal\Rfc4514;
+use PhpCfdi\Credentials\Tests\TestCase;
+
+class Rfc4514Test extends TestCase
+{
+    /**
+     * @param string $subject
+     * @param string $expected
+     * @dataProvider providerEscape
+     */
+    public function testEscape(string $subject, string $expected): void
+    {
+        $object = new Rfc4514();
+        $this->assertSame($expected, $object->escape($subject));
+    }
+
+    public function testEscapeArray(): void
+    {
+        $subject = [
+            'foo' => 'foo bar',
+            '#foo' => '#foo bar',
+            'foo ' => 'foo bar ',
+            'address' => 'Street #1, Somehere',
+        ];
+        $expected = implode(',', [
+            'foo=foo bar',
+            '\22foo=\22foo bar',
+            'foo\20=foo bar\20',
+            'address=Street #1\2c Somehere',
+        ]);
+
+        $object = new Rfc4514();
+        $this->assertSame($expected, $object->escapeArray($subject));
+    }
+
+    public function providerEscape()
+    {
+        return [
+            'normal' => ['foo bar', 'foo bar'],
+            'with lead space' => [' foo', '\20foo'],
+            'with double lead space' => ['  foo', '\20 foo'],
+            'with inner space' => ['foo bar', 'foo bar'],
+            'with trail space' => ['foo ', 'foo\20'],
+            'with lead #' => ['#foo bar', '\22foo bar'],
+            'with double lead #' => ['##foo', '\22#foo'],
+            'with inner #' => ['foo#bar', 'foo#bar'],
+            'with trail #' => ['foo bar#', 'foo bar#'],
+            'with =' => ['=foo=bar=', '\3dfoo\3dbar\3d'],
+            'complex => [# a=1,b>2,c<3 ]' => ['# a=1,b>2,c<3 ', '\22 a\3d1\2cb\3e2\2cc\3c3\20'],
+        ];
+    }
+}
