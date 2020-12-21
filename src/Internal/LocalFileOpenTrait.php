fb9c21f83bc146f8a542a3403ba009193af83cee
Carlos C Soto <eclipxe13@gmail.com>
2020-12-20 18:46:19 -0600

    Change exception message on file not found

--- src/Internal/LocalFileOpenTrait.php
+++ src/Internal/LocalFileOpenTrait.php
@@ -15,13 +15,23 @@ trait LocalFileOpenTrait
         if ('file://' === substr($filename, 0, 7)) {
             $filename = substr($filename, 7);
         }
+
+        if ('' === $filename) {
+            throw new UnexpectedValueException('The file to open is empty');
+        }
+
         $scheme = strval(parse_url($filename, PHP_URL_SCHEME));
-        if ('' !== $scheme) {
+        if ('' !== $scheme && strlen($scheme) > 1) {
             throw new UnexpectedValueException('Invalid scheme to open file');
         }
 
+        $path = (realpath($filename) ?: '');
+        if ('' === $path) {
+            throw new RuntimeException('Unable to locate the file to open');
+        }
+
         /** @noinspection PhpUsageOfSilenceOperatorInspection */
-        $contents = @file_get_contents($filename, false) ?: '';
+        $contents = @file_get_contents($path, false) ?: '';
         if ('' === $contents) {
             throw new RuntimeException('File content is empty');
         }
--- tests/Unit/Internal/LocalFileOpenTraitTest.php
+++ tests/Unit/Internal/LocalFileOpenTraitTest.php
@@ -47,7 +47,7 @@ class LocalFileOpenTraitTest extends TestCase
         $filename = __DIR__ . '/nonexistent';
         $specimen = new LocalFileOpenTraitSpecimen();
         $this->expectException(RuntimeException::class);
-        $this->expectExceptionMessage('File content is empty');
+        $this->expectExceptionMessage('Unable to locate the file to open');
         $specimen->localFileOpen($filename);
     }
 }
