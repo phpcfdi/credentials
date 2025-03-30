9b148b0f6688b384f971a0ed63df573bff192fa9
Carlos C Soto <eclipxe13@gmail.com>
2025-03-30 01:03:53 -0600

    Improve type declarations

--- src/Internal/BaseConverterSequence.php
+++ src/Internal/BaseConverterSequence.php
@@ -62,7 +62,7 @@ class BaseConverterSequence
         }
 
         $valuesCount = array_count_values(str_split(strtoupper($sequence)));
-        $repeated = array_filter($valuesCount, function (int $count) {
+        $repeated = array_filter($valuesCount, function (int $count): bool {
             return 1 !== $count;
         });
         if ([] !== $repeated) {
